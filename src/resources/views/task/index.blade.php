c8d14b16a7539cd3d3dcb53fd5e4638f846a9a9c
Ninjinman <tris.aisice@gmail.com>
2023-03-27 20:40:02 +0900

    taskwave

--- src/resources/views/task/index.blade.php
+++ src/resources/views/task/index.blade.php
@@ -4,48 +4,38 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ToDo管理アプリ</title>
+    <title>TaskWave</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <h1 class="logo">ToDo管理アプリ</h1>
+        <h1 class="logo">TaskWave</h1>
     </header>
     <main>
         <h1 class="title">タスク一覧</h1>
-        <form action="/tasks" method="post">
+        <form class="task-form" action="/tasks" method="post">
             @csrf
             <input class="task-input" type="text" name="task" placeholder="タスクを入力">
             <input class="task-input" type="text" name="description" placeholder="詳細">
             <button class="submit-button" type="submit">登録</button>
         </form>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>タスク名</th>
-                    <th>詳細</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                    <tr>
-                        <td>{{ $task->id }}</td>
-                        <td>{{ $task->title }}</td>
-                        <td>{{ $task->description }}</td>
-                        <td><a href="/tasks/{{ $task->id }}/edit">編集</a></td>
-                        <td>
-                            <form action="/tasks/{{ $task->id }}" method="post">
-                                @csrf
-                                @method('DELETE')
+        <div class="task-container">
+            @foreach ($tasks as $task)
+                <div class="task-card">
+                    <h3>{{ $task->title }}</h3>
+                    <p>{{ $task->description }}</p>
+                    <div class="task-actions">
+                        <a href="/tasks/{{ $task->id }}/edit" class="edit-button">編集</a>
+                        <form action="/tasks/{{ $task->id }}" method="post" class="delete-form">
+                            @csrf
+                            @method('DELETE')
                             <button class="delete-button" type="submit">削除</button>
-                </form>
-            </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </main>
     <footer class="footer">
         <!-- フッターナビゲーションバーの内容 -->
